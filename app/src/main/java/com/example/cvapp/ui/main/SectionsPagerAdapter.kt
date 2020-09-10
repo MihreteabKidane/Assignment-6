a295df5ece9e6a0350925b15fce933ba21b7bfd5
Brook <brook1kenfemichael@gmail.com>
2020-09-09 22:35:31 -0500

    Home Fragment Prepared

--- app/src/main/java/com/example/cvapp/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/cvapp/ui/main/SectionsPagerAdapter.kt
@@ -4,12 +4,8 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.cvapp.HomeFragment
 import com.example.cvapp.R
-
-private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
-)
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -21,11 +17,17 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        return when(position) {
+            0 -> HomeFragment.newInstance("Home", "param2")
+            else -> HomeFragment.newInstance("Home", "param2")
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+        return when(position) {
+            0 -> "Home"
+            else -> "Home"
+        }
     }
 
     override fun getCount(): Int {
