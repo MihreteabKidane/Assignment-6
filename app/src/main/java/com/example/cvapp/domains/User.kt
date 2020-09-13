a6441d79ff81b1d93ec105aa81740e8ddd85234c
Brook <brook1kenfemichael@gmail.com>
2020-09-13 16:37:16 -0500

    User Domain Created

---
+++ app/src/main/java/com/example/cvapp/domains/User.kt
@@ -0,0 +1,4 @@
+package com.example.cvapp.domains
+
+class User(var firstname : String, var lastname : String, var bio : String, var achievements : ArrayList<String>, var facebook : String, var twitter : String, var linkedIn : String, var personal : String) {
+}
